31f5bfb92841d58fcf1ab11854b13c1c3bee2fc7
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-20 22:55:54 +0100

    Add files via upload

---
+++ subs.s
@@ -0,0 +1,19 @@
+Compiled from "subs.java"
+public class subs {
+  public subs();
+    Code:
+         0: aload_0
+         1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+         4: return
+
+  public static void main(java.lang.String[]);
+    Code:
+         0: iconst_1
+         1: iconst_1
+         2: invokestatic  #7                  // Method subss:(II)I
+         5: istore_1
+         6: getstatic     #13                 // Field java/lang/System.out:Ljava/io/PrintStream;
+         9: iload_1
+        10: invokevirtual #19                 // Method java/io/PrintStream.println:(I)V
+        13: return
+}
