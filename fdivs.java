8d490d1ba56fb7f7a7faf563be8dcb666cc315e9
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-21 01:45:55 +0100

    Add files via upload

---
+++ fdivs.java
@@ -0,0 +1,9 @@
+public class fdivs {
+    private static float divss(float a,float b){
+        return a/b;
+    }
+    public static void main(String[] args) {
+        float a=divss(1,1);
+        System.out.println(a);
+    }
+}
