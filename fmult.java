15ff228aaade291f4c41a21766b45c97502f6a71
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-21 01:50:03 +0100

    Add files via upload

---
+++ fmult.java
@@ -0,0 +1,9 @@
+public class fmult {
+    private static float mul(float a,float b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        float a=mul(1,1);
+        System.out.println(a);
+    }
+}
