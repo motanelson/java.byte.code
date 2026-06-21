9bfb583f67533801c1994811532cbad7e47ad12b
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-21 01:54:44 +0100

    Add files via upload

---
+++ sHello.java
@@ -0,0 +1,8 @@
+public class sHello {
+    private static String hello(){
+       return "hello world!";
+    }
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+}
