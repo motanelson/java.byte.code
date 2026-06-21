67741675b0495ab771680561baca551a1e538f55
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-21 01:39:41 +0100

    Add files via upload

---
+++ fsubs.java
@@ -0,0 +1,9 @@
+public class fsubs {
+    private static float subss(float a,float b){
+        return a-b;
+    }
+    public static void main(String[] args) {
+        float a=subss(1,1);
+        System.out.println(a);
+    }
+}
