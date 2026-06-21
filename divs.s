9af1d67b9ae7c9316b554c850ec959e3abf20c90
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-21 01:26:18 +0100

    Add files via upload

--- divs.s
+++ divs.s
@@ -6,6 +6,13 @@ public class divs {
          1: invokespecial #1                  // Method java/lang/Object."<init>":()V
          4: return
 
+  private static int divss(int, int);
+    Code:
+         0: iload_0
+         1: iload_1
+         2: idiv
+         3: ireturn
+
   public static void main(java.lang.String[]);
     Code:
          0: iconst_1
