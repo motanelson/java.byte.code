882acbbeb236860600f02829df933f2cff6926bb
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-10 07:10:26 +0100

    Delete subs.java

--- subs.java
+++
@@ -1,9 +0,0 @@
-public class subs {
-    private static int subss(int a,int b){
-        return a-b;
-    }
-    public static void main(String[] args) {
-        int a=subss(1,1);
-        System.out.println(a);
-    }
-}
