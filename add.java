0bd8915607d7f14f101d9df0831fc42117cc4917
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-10 07:18:08 +0100

    Delete add.java

--- add.java
+++
@@ -1,9 +0,0 @@
-public class add {
-    private static int adds(int a,int b){
-        return a+b;
-    }
-    public static void main(String[] args) {
-        int a=adds(1,1);
-        System.out.println(a);
-    }
-}
