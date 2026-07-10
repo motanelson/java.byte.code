5861e059a3b806fd5e0973284c1aa24fd5e131f8
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-10 07:21:10 +0100

    Change hello method to return colored output

--- sHello.java
+++ sHello.java
@@ -1,6 +1,6 @@
 public class sHello {
     private static String hello(){
-       return "hello world!";
+       return "\033[47;30m\nhello world!";
     }
     public static void main(String[] args) {
         System.out.println(hello());
