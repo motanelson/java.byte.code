3aaf378b44fc977ccc9f15e482584ba4513840ac
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-10 07:15:38 +0100

    Delete fadd.java

--- fadd.java
+++
@@ -1,9 +0,0 @@
-public class fadd {
-    private static float adds(float a,float b){
-        return a+b;
-    }
-    public static void main(String[] args) {
-        float a=adds(1,1);
-        System.out.println(a);
-    }
-}
