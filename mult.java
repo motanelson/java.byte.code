b918164c008182ebcfc51c2e4353d529fdf91a07
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-10 07:12:00 +0100

    Delete mult.java

--- mult.java
+++
@@ -1,9 +0,0 @@
-public class mult {
-    private static int mul(int a,int b){
-        return a*b;
-    }
-    public static void main(String[] args) {
-        int a=mul(1,1);
-        System.out.println(a);
-    }
-}
