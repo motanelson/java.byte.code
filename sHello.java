7e3f239df44f471e93bb6fd9948d21fd142a550c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-11 07:11:41 +0100

    Delete sHello.java

--- sHello.java
+++
@@ -1,8 +0,0 @@
-public class sHello {
-    private static String hello(){
-       return "\033[47;30m\nhello world!";
-    }
-    public static void main(String[] args) {
-        System.out.println(hello());
-    }
-}
